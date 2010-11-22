4cfda8bfca5d59d0a50969ee04b7482d28e5e15c
Pratik Khadloya <tispratik@gmail.com>
2010-11-22 16:37:27 -0600

    first commit

---
+++ generate_dependency_tree_graph.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  
+# This creates a gif file for every package installed package
+# that dpkg is aware of.
+
+# You may need to install these packages
+# sudo apt-get install apt-rdepends
+# sudo apt-get install graphviz
+
+for ii in $(  dpkg -l | awk '{print $2}' ); do
+          apt-rdepends -d $ii > $ii.dotty
+          dot -Gratio=auto -Tgif -o $ii-dependency.gif $ii.dotty
+          rm -f $ii.dotty
+done
