b72ee872b51de223e9eda4cb464a76318960230c
Pratik Khadloya <tispratik@gmail.com>
2011-02-11 13:05:07 -0600

    Open terminal script

---
+++ launch_terminals.sh
@@ -0,0 +1,4 @@
+gnome-terminal -t "home" --working-directory ~ --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "rails_logger" --working-directory ~/Projects/rails_logger --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "inventory" --working-directory ~/Projects/inventory --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "scripts" --working-directory ~/Auxiliary\ Projects/scripts --hide-menubar --window-with-profile=pratik --geometry=180x40 &
