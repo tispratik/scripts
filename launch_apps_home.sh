6cb2d6102718125ddc3d72e16d27a472a8a4bdbd
Pratik Khadloya <tispratik@gmail.com>
2011-02-12 00:53:58 -0600

    home apps

---
+++ launch_apps_home.sh
@@ -0,0 +1,14 @@
+# Terminals
+gnome-terminal -t "home" --working-directory ~ --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "bs" --working-directory ~/Projects/bs --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "pratik_rackspace" --working-directory ~/Projects/bs --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "deploy_rackspace" --working-directory ~/Projects/bs --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "downloads" --working-directory ~/Downloads --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+gnome-terminal -t "dotfiles" --working-directory ~/Auxiliary\ Projects/dotfiles --hide-menubar --window-with-profile=pratik --geometry=180x40 &
+
+# File Browsers
+nautilus ~
+nautilus ~/Projects/bs
+nautilus ~/Auxiliary\ Projects
+nautilus ~/Downloads
+nautilus /d
