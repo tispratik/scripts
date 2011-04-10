ca69bfc1591d3d065050ff1011182808bd6ff7d9
Pratik Khadloya <tispratik@gmail.com>
2011-04-10 16:13:00 -0500

    capistrano_database file added

---
+++ capistrano/capistrano_database.rb
@@ -0,0 +1,45 @@
+Capistrano::Configuration.instance.load do
+  namespace :deploy do
+    namespace :db do
+      desc <<-DESC
+        Creates the database.yml configuration file in shared path.
+        By default, this task uses a template unless a template called database.yml.erb is found either is :template_dir
+        or /config/deploy folders. The default template matches the template for config/database.yml file shipped with Rails.
+      DESC
+      task :setup, :except => { :no_release => true } do
+
+	set(:db_username, Capistrano::CLI.ui.ask("MySQL application user: "))
+	set(:db_password, Capistrano::CLI.password_prompt("MySQL application password: "))  
+
+        default_template = <<-EOF
+base: &base
+  adapter: mysql
+  timeout: 5000
+production:
+  database: #{application}_test
+  username: #{db_username}
+  password: #{db_password}
+  <<: *base
+        EOF
+
+        location = fetch(:template_dir, "config/deploy") + '/database.yml.erb'
+        template = File.file?(location) ? File.read(location) : default_template
+
+        config = ERB.new(template)
+	run "mkdir -p #{shared_path}/config"
+        put config.result(binding), "#{shared_path}/config/database.yml"
+      end
+
+      desc <<-DESC
+        [internal] Updates the symlink for database.yml file to the just deployed release.
+      DESC
+      task :symlink, :except => { :no_release => true } do
+        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+      end
+    end
+
+    after "deploy:setup",           "deploy:db:setup"   unless fetch(:skip_db_setup, false)
+    after "deploy:finalize_update", "deploy:db:symlink"
+
+  end
+end
--- capistrano/deploy.rb
+++ capistrano/deploy.rb
@@ -2,6 +2,7 @@
 $:.unshift(File.expand_path("~/.rvm/lib"))
 require 'rvm/capistrano'
 
+set :rvm_ruby_string, '1.9.2@coeditr'	# Required so that RVM finds bundler gem in the environment and bundler installs them to that gemset
 set :rvm_type, :user # :system or :user – user looks for rvm in $HOME/.rvm where as system uses the /usr/local as set for system wide installs.
 
 # Bundler Bootstrap
@@ -17,9 +18,12 @@ role :web, domain.to_s
 role :db, domain.to_s, :primary => true 
 
 # server details
-ssh_options[:port] = XXX	# If you have SSH server running on non standard port
-set :user, "<deployment_user>"
+#ssh_options[:port] = XXX		# If you have SSH server running on non standard port
+set :ssh_options, { :port => XXX }	# Another way, you can pass multiple parameters
+
+set :user, "<deployment_user>"	# The SSH username you are logging into the server(s) as.
 set :use_sudo, false
+
 default_run_options[:pty] = true
 #ssh_options[:forward_agent] = true
 
@@ -29,7 +33,7 @@ set :scm_username, "xxx@gmail.com"			# If using Github
 set :repository, "git@github.com:tispratik/scripts.git"	# Repo path
 set :branch, "master"
 
-set :deploy_to, "/home/<deployment_user>/public_html/<app_name>"
+set :deploy_to, "/home/#{user}/public_html/#{application}"	# The path on the servers we’re going to be deploying the application to.
 set :deploy_via, :remote_cache
 #set :deploy_via, :copy
 #set :git_enable_submodules, 1
@@ -43,26 +47,10 @@ def run_my_task(task)
   run "cd #{current_path} && rake #{task} RAILS_ENV=production"
 end
 
-# Database
-namespace :db do
-  task :symlink, :roles => :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-end
-
 namespace :log do
   task :prod do
     run "tail -n 200 -f #{shared_path}/log/production.log"
   end
 end
  
-namespace :deploy do 
-  %w(start stop restart).each do |action| 
-    desc "#{action} our server" 
-    task action.to_sym do 
-      find_and_execute_task("#{server_type}:#{action}") 
-    end 
-  end
-end
-
-after 'deploy:update_code', 'db:symlink'
+after 'deploy:update_code', 'deploy:db:symlink'
