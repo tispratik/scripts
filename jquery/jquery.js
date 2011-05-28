66950cca0d68413611a35905fcde808d73feb806
Pratik Khadloya <tispratik@gmail.com>
2011-05-28 08:55:47 -0500

    Updates

--- jquery/jquery.js
+++
@@ -1,8 +0,0 @@
-# Script to append the first element of a list to the end
-$('#nav ul li:first').appendTo('#nav ul');
-
-# Copy first element in the list to the end of the list
-$('#nav ul li:first').clone().appendTo('#nav ul');
-
-# Copy first element in the list to the end of the list along with the related data and events
-$('#nav ul li:first').clone(true).appendTo('#nav ul');
